d60dac427594f98ece8624dcbb1c5a5ffc96e364
slllldka <sne0103@naver.com>
2024-10-29 14:58:55 +0900

    backend: added phonenumber to User

--- server/account/serializers.py
+++ server/account/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ("__all__")
+        fields = ('id', 'username', 'phonenumber', 'first_name', 'last_name')
         #fields = ('name', 'description', 'cost')
