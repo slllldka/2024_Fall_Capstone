b779f8bc761577af34b56f83fc62aef5bf867f7b
elect10 <wlgnsakswp@naver.com>
2024-10-29 09:33:35 +0900

    chore(infra): setting

---
+++ fn/src/auth/pages/Login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export default function Login(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Login</Text>
+    </SafeAreaView>
+  );
+}
---
+++ fn/src/auth/pages/Register.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
+
+export default function Register(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Reg</Text>
+    </SafeAreaView>
+  );
+}
